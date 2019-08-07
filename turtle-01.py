078bb202be089f09ce8e4e2542f3218737856226
zxxd <44012354+zxxd@users.noreply.github.com>
2019-08-07 11:50:23 +0800

    Create turtle-01.py

---
+++ turtle-01.py
@@ -0,0 +1,17 @@
+#持续学习python的第三天，推荐了GitHub的python100天，今天是第一天
+#学习使用turtle在屏幕上绘制图形，很想问问这个包为什么要叫turtle
+
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
+
+#forward是前进的意思，整部代码为前进100，向右90度，前进100，向右90度，循环往复形成一个正方形
